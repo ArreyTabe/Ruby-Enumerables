387094022105f56152f68e2025a4a262dc07533c
Arrey Tabe <arrey2391@gmail.com>
2020-11-11 11:16:31 +0100

    implements my_each_with_index method

--- script.rb
+++ script.rb
@@ -11,7 +11,7 @@ module Enumerable
         end
        self
     end
-end
+
 
 test_collect = ["goond","young", "lappy", "shangai"]
 
@@ -20,4 +20,24 @@ test_collect.my_each do |el|
     puts "The test array contains the nonsense #{el}"
 
     
+end
+
+# my_each_with_index
+
+def my_each_with_index
+    return self.dup unless block_given?
+      i = 0 
+      until i == self.size
+          yield self[i]
+          i +=1
+      end
+      self
+  end
+end
+
+
+test_collect = [23,34,56,78,45]
+
+test_collect.my_each_with_index  do |el,index|
+   puts "index #{index} for #{el}" 
 end
