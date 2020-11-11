36699f23a1df453a8d73d93a8a54473997aab81d
oluwadare juwon <wintan1418@gmail.com>
2020-11-11 12:10:47 +0100

    implement the select method on a given set of array

--- script.rb
+++ script.rb
@@ -33,11 +33,32 @@ def my_each_with_index
       end
       self
   end
-end
+
 
 
 test_collect = [23,34,56,78,45]
 
 test_collect.my_each_with_index  do |el,index|
    puts "index #{index} for #{el}" 
+end
+
+def my_select
+    return self.dup unless block_given?
+    selected_array = []
+    i = 0 
+    until i == self.size
+        selected_array<< yield(self[i])
+    i +=1
+  end
+  selected_array
+
+end
+end
+
+  test_collect = ["binta", "gabru", "sharon"]
+
+  test_collect.my_select do |el|
+      puts el if el != "sharon"
+
+
 end
