9f47a6105d7c8d18966c94347a77de6fe6ea9278
Arrey Tabe <arrey2391@gmail.com>
2020-11-17 09:27:37 +0100

    fixes linters errors

--- script.rb
+++ script.rb
@@ -1,121 +1,121 @@
- public
+public
+
  module Enumerable
+   def my_each
+     return to_enum(:my_each) unless block_given?
 
-  def my_each
-    return to_enum(:my_each) unless block_given?
+     arr = self
+     arr = arr.to_a if arr.instance_of?(Range)
+     arr = arr.to_a if arr.instance_of?(Hash)
+     i = 0
+     until i == size
+       yield arr[i]
 
-    arr = self
-    arr = arr.to_a if arr.instance_of?(Range)
-    arr = arr.to_a if arr.instance_of?(Hash)
-    i = 0
-    until i == size
-      yield arr[i]
+       i += 1
+     end
+     self
+   end
 
-      i += 1
-    end
-    self
-  end
+   # my_each_with_index
 
-  # my_each_with_index
+   def my_each_with_index
+     return to_enum(:my_each_with_index) unless block_given?
 
-  def my_each_with_index
-    return to_enum(:my_each_with_index) unless block_given?
+     arr = *self
+     i = 0
+     until i == size
+       yield(arr [i], i)
+       i += 1
+     end
+     self
+   end
 
-    arr = *self
-    i = 0
-    until i == size
-      yield(arr [i], i)
-      i += 1
-    end
-    self
-  end
+   # my_select
 
-  # my_select
+   def my_select
+     return to_enum(:my_select) unless block_given?
 
-  def my_select
-    return to_enum(:my_select) unless block_given?
+     selected_array = []
+     arr = to_a
+     i = 0
+     until i == size
+       selected_array << arr[i] if yield(arr[i])
+       i += 1
+     end
+     selected_array
+   end
 
-    selected_array = []
-    arr = to_a
-    i = 0
-    until i == size
-      selected_array << arr[i] if yield(arr[i])
-      i += 1
-    end
-    selected_array
-  end
+   # my_all
 
-  # my_all
+   def my_all?(arg = nil)
+     if block_given?
+       my_each { |el| return false if yield(el) == false }
 
-  def my_all?(arg = nil)
-    if block_given?
-      my_each { |el| return false if yield(el) == false }
+       return true
+     elsif arg.nil?
+       my_each { |k| return false if k.nil? || k == false }
+     elsif !arg.nil? && (arg.is_a? Class)
+       my_each { |k| return false if k.class != arg }
+     elsif !arg.nil? && arg.instance_of?(Regexp)
+       my_each { |k| return false unless arg.match(k) }
+     else
+       my_each { |k| return false if k != arg }
+     end
+     true
+   end
 
-      return true
-    elsif arg.nil?
-      my_each { |k| return false if k.nil? || k == false }
-    elsif !arg.nil? && (arg.is_a? Class)
-      my_each { |k| return false if k.class != arg }
-    elsif !arg.nil? && arg.instance_of?(Regexp)
-      my_each { |k| return false unless arg.match(k) }
-    else
-      my_each { |k| return false if k != arg }
-    end
-    true
-  end
+   #  my_any
 
-  #  my_any
+   def my_any?(arg = nil)
+     if block_given?
+       my_each { |el| return true if yield(el) }
+       return false
+     end
+     arg.nil? ? arg.class.to_s : my_any? { |el| el }
 
-  def my_any?(arg = nil)
-    if block_given?
-      my_each { |el| return true if yield(el) }
-      return false
-    end
-    arg.nil? ? arg.class.to_s : my_any? { |el| el }
+     if arg.class.to_s == 'Class'
+       my_each { |el| return true if el.is_a? arg }
+     elsif arg.class.to_s == 'Regexp'
+       my_each { |el| return true if el =~ arg }
+     elsif arg.nil?
+       each { |el| return true if el }
+     else
+       my_each { |el| return true if el == arg }
+     end
+     false
+   end
 
-    if arg.class.to_s == 'Class'
-      my_each { |el| return true if el.is_a? arg }
-    elsif arg.class.to_s == 'Regexp'
-      my_each { |el| return true if el =~ arg }
-    elsif arg.nil?
-      each { |el| return true if el }
-    else
-      my_each { |el| return true if el == arg }
-    end
-    false
-  end
+   # my_none
 
-  # my_none
+   def my_none?(arg = nil, &block)
+     !my_any?(arg, &block)
+   end
 
-  def my_none?(arg = nil, &block)
-    !my_any?(arg, &block)
-  end
+   # my_count
 
-  # my_count
+   def my_count(*arg)
+     count = 0
+     if block_given?
+       my_each do |ele|
+         count += 1 if yield(ele)
+       end
+       count
 
-  def my_count(*arg)
-    count = 0
-    if block_given?
-      my_each do |ele|
-        count += 1 if yield(ele)
-      end
-      count
+     elsif arg.length.positive?
 
-    elsif arg.length.positive?
-
-      my_each do |ele|
-        count += 1 if ele == arg[0]
-      end
-      count
-    elsif arg.length.zero? && !block_given?
-      count = 0
-      my_each do |_ele|
-        count += 1
-      end
-    end
-    count
-  end
-end
+       my_each do |ele|
+         count += 1 if ele == arg[0]
+       end
+       count
+     elsif arg.length.zero? && !block_given?
+       count = 0
+       my_each do |_ele|
+         count += 1
+       end
+     end
+     count
+   end
+ end
 
 # my_map
 
