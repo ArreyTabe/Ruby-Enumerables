a392e514e463197264b90b9f8b1167d1aa14bee1
uchennaanya <uchennaongit@gmail.com>
2020-11-30 11:15:08 +0100

    Add the my_each_with_index test

--- spec/script_spec.rb
+++ spec/script_spec.rb
@@ -9,7 +9,16 @@ describe Enumerable do
     it 'iterates through each item in the collection if block is given returns enum otherwise' do
       expect(arr.my_each).to be_instance_of(Enumerator)
       expect(hash.my_each).to be_instance_of(Enumerator)
+      expect(arr.my_each {|n| n }).to be_instance_of(Array)
       expect(range.my_each { |n| n * 2 }).not_to be_instance_of(Array)
     end
   end
+  describe "#my_each_with_index" do
+      it "Iterate through the elements in a collection and returns the elements with its index if block is given, otherwise it returns the Enum" do
+       expect(arr.my_each_with_index).to_not be_instance_of(Array)
+       expect(astring.my_each_with_index).to be_instance_of(Enumerator) 
+       expect(astring.my_each_with_index { |x, y| puts x, y }).not_to eql({ "this" => 0, "is" => 1, "a" => 2, "string" => 3  })
+       expect([astring, 'uchenna'].my_each_with_index(3) { |e, i| puts e, i }).to raise_error(ArgumentError)
+       end
+    end
 end
