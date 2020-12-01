f970c2de9b7e9e0466f772a62c219abef97adfbb
Arrey Tabe <arrey2391@gmail.com>
2020-12-01 04:46:54 +0100

    adds more tests for our enum methods

--- spec/script_spec.rb
+++ spec/script_spec.rb
@@ -45,7 +45,7 @@ describe Enumerable do
     it 'iterates a collection and checks the condition within a given block' do
       expect(%w[is almost done].my_any? { |n| n.length > 4 }).to eq(true)
     end
-    it 'checks for any boolean type' do 
+    it 'checks for any boolean type' do
       expect([true, 44, 'yes'].my_any?).to eq(true)
     end
     it 'checks if there are elements in the Array' do
@@ -53,34 +53,68 @@ describe Enumerable do
     end
   end
 
-  describe "#my_none?" do
-      it "check condition inside a block" do
-        expect(astring.my_none? { |word| word.length == 5 }).to eq(astring.none? { |word| word.length == 5 })
-      end
-      it 'check if one of the elements is equal to the specified class' do
-        expect([1, 3.14, 42].my_none?(Float)).to eq([1, 3.14, 42].none?(Float))
-      end
-      it 'check if the array is empty' do
-        expect([].my_none?).to eq([].none?)
-      end
-  end
-  
-  describe "#my_count" do
-    it "evalute the method" do
-        expect(arr.my_count).to eq(arr.my_count)
+  describe '#my_none?' do
+    it 'check condition inside a block' do
+      expect(astring.my_none? { |word| word.length == 5 }).to eq(astring.none? { |word| word.length == 5 })
     end
-    it "evaluate the my_count method" do
-        expect(arr.my_count(2)).to eq(arr.count(2))
+    it 'check if one of the elements is equal to the specified class' do
+      expect([1, 3.14, 42].my_none?(Float)).to eq([1, 3.14, 42].none?(Float))
+    end
+    it 'check if the array is empty' do
+      expect([].my_none?).to eq([].none?)
     end
   end
 
-  describe "#my_map" do
-      it "loops through an array and returns return new array with the mapped element" do
-          expect(range.my_map {|i| i * 3 }).to eq(range.map { |i| i * 3})
-      end
-      it 'returns array with new results' do
-        expect(range.my_map { 'cat' }).to eq(range.map { 'cat' })
-      end
+  describe '#my_count' do
+    it 'evalute the method' do
+      expect(arr.my_count).to eq(arr.my_count)
+    end
+    it 'evaluate the my_count method' do
+      expect(arr.my_count(2)).to eq(arr.count(2))
+    end
   end
 
+  describe '#my_map' do
+    it 'loops through an array and returns new array with the mapped element' do
+      expect(range.my_map { |i| i * 3 }).to eq(range.map { |i| i * 3 })
+    end
+    it 'returns array with new results' do
+      expect(range.my_map { 'cat' }).to eq(range.map { 'cat' })
+    end
+  end
+
+  describe '#my_inject' do
+    it 'checks my_inject method' do
+      expect((5..10).my_inject(:+)).to eq(45)
+    end
+    it 'runs throung a block summing it' do
+      expect((5..10).my_inject { |sum, n| sum + n }).to eq(45)
+    end
+    it 'multiplies some numbers' do
+      expect((5..10).my_inject(1, :*)).to eq(151_200)
+    end
+    it 'iterates over a collection of words and returns the longest word' do
+      expect(%w[cat sheep bear].my_inject do |memo, word|
+               memo.length > word.length ? memo : word
+             end).to eq('sheep')
+    end
+  end
+
+  describe '#my_count' do
+    ary = [1, 2, 4, 2]
+    it 'evaluates my_count method' do
+      expect(ary.my_count).to eq(4)
+    end
+    it 'evaluates my count method' do
+      expect(ary.my_count(2)).to eq(2)
+    end
+    it 'runs loops through a collection' do
+      expect(ary.my_count { |x| x.even? }).to eq(3)
+    end
+  end
+  describe '#multiply_else' do
+    it 'evaluates multiply else method' do
+      expect(multiply_els([1, 2, 3, 4])).to eq(24)
+    end
+  end
 end
